22ae3920dd1bd02237ecbc89363e4c74bf2c099b
Anthony Hill <anthonymhill23@gmail.com>
2022-04-01 18:31:13 -0400

    refactored code to  incorporte auth0 as well as cleaned up best books carousel

--- src/BookCarousel.js
+++ src/BookCarousel.js
@@ -1,30 +1,25 @@
 import React, { Component } from "react";
-import { Carousel,Card,Button } from "react-bootstrap";
+import { Carousel,Card } from "react-bootstrap";
 import BookInfo from "./BookInfo";
-import './BookCarousel.css'
-
 export default class BookCarousel extends Component {
+  
   
   render() {
     return (
       <Card>
         <Carousel>
-          {/* {this.state.books.map((book) => <Carousel.Item><Carousel.Caption>{book.title}</Carousel.Caption></Carousel.Item>) } */}
-          
           {this.props.books.map((book) => (
             <Carousel.Item>
-              <h1>{book._id}</h1>
-              <BookInfo key={book._id} book={book} deleteBook={this.props.deleteBook} updateBook={()=> this.props.updateBook(book._id)}  books={this.props.books}  showModal={this.props.showModal} onClick={this.handleClick} closeModal={this.closeModal} postBook={this.postBook}/>
-              <Button onClick={() => this.props.deleteBook(book._id)} variant='danger'>
-          Delete
-        </Button>
-        
-        <Button onClick={() => this.props.updateBook(book._id)} variant='primary'>
-          Update
-        </Button>
-              <Carousel.Caption>
-                
-              </Carousel.Caption>
+              
+              <BookInfo 
+                key={book._id} 
+                book={book} 
+                deleteBook={this.props.deleteBook} 
+                updateBook={this.props.updateBook}  
+                books={this.props.books}
+                postBook={this.props.postBook}
+              />
+              
             </Carousel.Item>
           ))}
         </Carousel>
